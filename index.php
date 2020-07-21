5e39692c819b2dd1b6ebd86f7bec9622693dd598
Zakodro4 <60068872+Zakodro4@users.noreply.github.com>
2020-07-21 18:00:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ include "vendor/autoload.php";
 
 $amino = new Amino("email", "password");
 $auth = $amino->auth();
-$client = new WebSocket\Client("wss://ws1.narvii.com?signbody=015051B67B8D59D0A86E0F4A78F47367B749357048DD5F23DF275F05016B74605AAB0D7A6127287D9C%7C".(time()*1000)."&sid=".$auth["sid"]);
 while (true) {
+    $client = new WebSocket\Client("wss://ws1.narvii.com?signbody=015051B67B8D59D0A86E0F4A78F47367B749357048DD5F23DF275F05016B74605AAB0D7A6127287D9C%7C".(time()*1000)."&sid=".$auth["sid"]);
     try {
         $result = json_decode($client->receive(),true);
         if($result["t"] == 1000){ // Если пришло новое сообщение
